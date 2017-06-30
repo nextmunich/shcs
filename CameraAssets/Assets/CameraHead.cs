ed3e39f93558bbc4ba0e75b5b1d58cde7897fe3f
= <ammar.hadzic@gmail.com>
2017-06-30 23:23:46 +0200

    Inital commimt for cameraS

---
+++ CameraAssets/Assets/CameraHead.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHead : MonoBehaviour {
+
+	[System.NonSerialized]
+	public int TargetedRotationAngle = 180;
+	public int RotationSpeed = 4;
+	public bool IsRotetable = false;
+	[System.NonSerialized]
+	public bool RotateOnlyOneDirection = false;
+	private bool directionPositive = true;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsRotetable) {
+			Debug.Log (gameObject.transform.eulerAngles.y);
+			Debug.Log (directionPositive);
+			int yAngle = (int)gameObject.transform.eulerAngles.y;
+			if (directionPositive) {
+				if (yAngle != TargetedRotationAngle / 2 || RotateOnlyOneDirection) {
+					gameObject.transform.Rotate (0, TargetedRotationAngle * Time.deltaTime / 10, 0);
+				} else {
+					directionPositive = false;
+				}
+			} else {
+				if (yAngle !=  360 - TargetedRotationAngle / 2) {
+					gameObject.transform.Rotate (0, TargetedRotationAngle * Time.deltaTime / -10, 0);
+				} else {
+					directionPositive = true;
+				}
+			}
+		} 
+	}
+}
---
+++ CameraAssets/Assets/SmartCameraObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartCameraObject : MonoBehaviour {
+
+	// Use this for initialization
+	public float Red;
+	public float Green;
+	public float Blue;
+
+	public bool AlreadyColored = false;
+
+	public bool IsRotateable = false;
+
+	public CameraHead Head;
+
+	private int rotationAngle = 0;
+
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Head.IsRotetable = IsRotateable;
+
+		if (!AlreadyColored) {
+			var objs = FindObjectsOfType<ColorableObject> ();
+			foreach (var o in objs) {
+				o.Red = Red;
+				o.Green = Green;
+				o.Blue = Blue;
+				o.AlreadyColored = false;
+				AlreadyColored = true;
+			}
+		}
+	}
+
+	public void SetRotationAngle(int degree){
+		if (IsRotateable) {
+			rotationAngle = degree;
+
+		}
+	}
+}
