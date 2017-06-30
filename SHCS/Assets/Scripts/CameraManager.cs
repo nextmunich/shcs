a47eefa30d62b5cc4ed377a6b4d19d93970e0ae8
HoloLens Hackathon <fl@next-munich.com>
2017-07-01 01:26:04 +0200

    Add HoloToolkit examples

---
+++ SHCS/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class CameraManager : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        GazeManager.Instance.FocusedObjectChanged += GazeManagerInstance_FocusedObjectChanged;
+	}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    
+    
+    private void GazeManagerInstance_FocusedObjectChanged(GameObject previousObject, GameObject newObject)
+    {
+        var indoorCamera = newObject.GetComponent<IndoorCamera>();
+        if (indoorCamera != null && !indoorCamera.GetComponent<Rigidbody>())
+        {
+            var rigidBody = indoorCamera.gameObject.AddComponent<Rigidbody>();
+            rigidBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
